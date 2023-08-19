049daeef3c6ecff8253426e4649bccf9302d3be0
lucas <luckericksen@gmail.com>
2023-08-19 17:44:02 -0300

    :bug: fix seed admin

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,10 @@
 import 'dotenv/config'
 import { DataSource } from 'typeorm';
+import { User } from '../modules/accounts/entities/user';
+import { Demand } from '../modules/demand/entities/Demand';
+import { Service } from '../modules/services/entities/service';
+import { Client } from '../modules/clients/entities/Client';
+import { Reports } from '../modules/reports/entities/report';
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -9,7 +14,7 @@ const AppDataSource = new DataSource({
   password: String(process.env.DATABASE_PASSWORD),
   database: String(process.env.DATABASE_DATABASE),
   migrations: ["src/database/migrations/*.ts"],
-  entities: ["src/modules/**/entities/*.ts"],
+  entities: [User, Demand, Service, Reports, Client],
   logging: true,
 })
 
--- src/shared/typeorm/seed/admin.ts
+++ src/shared/typeorm/seed/admin.ts
@@ -1,23 +1,25 @@
 import { v4 as uuid } from "uuid"
 import { hash } from "bcryptjs"
 
-import createConnection from "../index"
+import AppDataSource from "../../../database";
+import { User } from "../../../modules/accounts/entities/user";
 
 async function create() {
+  await AppDataSource.initialize()
+  const repository = AppDataSource.getRepository(User)
 
-  const connection = await createConnection("localhost");
-  const id = uuid();
+  const userAdminExists = await repository.findOneBy({ isAdmin: true })
 
-  const password = await hash(process.env.PASSWORD, 10);
+  if (!userAdminExists) {
+    const id = uuid();
 
-  await connection.query(
-    `INSERT INTO  USERS(id, name, email, password, "isAdmin", created_at, updated_at)
-            values('${id}', '${process.env.USER}', '${process.env.USER_EMAIL}', '${password}', 'true', 'now()', 'now()')`
-  )
+    const password = await hash(process.env.PASSWORD, 10);
 
-  connection.close;
+    const newUserAdmin = repository.create({ id, name: String(process.env.USER), email: String(process.env.USER_EMAIL), password, isAdmin: true, created_at: new Date(), updated_at: new Date() })
 
+    await repository.save(newUserAdmin)
+  }
 
 }
 
-create().then(() => console.log("User Admin created!"))
+create().then(() => console.log("seed admin executada!")).catch(e => console.log("erro ao executar a seed:admin: " + e.message))
