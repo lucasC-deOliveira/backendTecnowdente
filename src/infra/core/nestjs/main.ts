4f72d34a52367624fe7b05429a9de3c2d0439892
lucas <luckericksen@gmail.com>
2023-10-28 12:01:30 -0300

    :hammer: cors all

--- src/infra/core/nestjs/main.ts
+++ src/infra/core/nestjs/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', String(process.env.FRONT_END));
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
